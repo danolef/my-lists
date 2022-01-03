6411cb111aa6de0bbc6c52c1c70605b18ad9acaa
Dan <dt.lefever@gmail.com>
2022-01-03 13:55:49 -0700

    added authentication and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authenticate_user
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+  
+  def authenticate_user
+    return render json: {error: "Not Authorized"}, status: :unauthorized unless current_user
+  end
+
+  private
+
+  def render_not_found(excepetion)
+    render json: {error: "#{excepetion.model} not found"}, status: :not_found
+  end
+
+  def render_invalid(excepetion)
+    render json: {errors: excepetion.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+skip_before_action :authenticate_user, only: [:create, :show]    
+
+def create
+    user= User.create(user.params)
+    session[:user_id] = user.id
+    render json: user, status: :ok
+end
+
+def show
+    if current_user
+        render json: current_user, status: :ok
+    else
+        render json: "No current session stored", status: :unauthorized
+    end
+end
+
+private
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+
 end
