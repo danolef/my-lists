8374ec0bce017fffea783ecbe7f614e677bb3198
Bubba Godinet <godinetvn@gmail.com>
2022-01-03 12:56:17 -0700

    switching branches

--- client/src/LoginSignupPage.js
+++ client/src/LoginSignupPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+
 
 export default function LoginSignupPage() {
     return(
         <>
-        <Button variant='contained'></Button>
         </>
     )
 }
