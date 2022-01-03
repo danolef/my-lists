47a58e648a676d21c16a6a8681e75a81106f4120
Bubba Godinet <godinetvn@gmail.com>
2022-01-03 12:16:29 -0700

    peerDependency

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import LoginSignupPage from './LoginSignupPage';
 
 function App() {
+
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
--- client/src/LoginSignupPage.js
+++ client/src/LoginSignupPage.js
@@ -1,5 +1,9 @@
+import Button from '@mui/material/Button';
+
 export default function LoginSignupPage() {
     return(
-        <></>
+        <>
+        <Button></Button>
+        </>
     )
 }
