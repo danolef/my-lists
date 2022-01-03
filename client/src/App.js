bf4c25b291dff6ee74d27fd988ff08f87ffdcf52
Bubba Godinet <godinetvn@gmail.com>
2022-01-03 11:20:39 -0700

    adjust dio

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LoginSignupPage from './LoginSignupPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <LoginSignupPage/>
     </div>
   );
 }
---
+++ client/src/HomePage.js
@@ -0,0 +1,13 @@
+import Header from './header'
+import Menu from './menu'
+import RotatingList from './RotatingList'
+
+export default function HomePage(){
+    return(
+        <>
+        <Header/>
+        <Menu/>
+        <RotatingList/>
+        </>
+    )
+}
