ada5e7a7596ee947e23b17f667b7c5e8e45c0b13
Bubba Godinet <godinetvn@gmail.com>
2022-01-03 12:40:07 -0700

    fixed mui problem

--- client/src/LoginSignupPage.js
+++ client/src/LoginSignupPage.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import Button from '@mui/material/Button';
 
 export default function LoginSignupPage() {
     return(
         <>
-        <Button></Button>
+        <Button variant='contained'></Button>
         </>
     )
 }
