304da133a4053a85760290feeb3332920881a3a4
Bubba Godinet <godinetvn@gmail.com>
2022-01-04 13:35:48 -0700

    finished a rough draft of the list page and list items to go on page. I cannot fully do it until the backend is fully setup

---
+++ client/src/ListItem.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function ListItem(){
+
+    function handleItemCardClick(){
+        console.log('you clicked the card')
+    }
+
+    function handleItemButtonClick(){
+        console.log('youclicked the item card button')
+    }
+        return (
+            <Card onClick={handleItemCardClick} sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="test image"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Item name
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Price
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button onClick={handleItemButtonClick} size="small">...</Button>
+              </CardActions>
+            </Card>
+          );
+        }
